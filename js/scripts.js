d541d941cf02a4b493dbd1f280cda4c287d31906
Michael Burton <micbur1@gmail.com>
2021-04-05 16:33:44 -0700

    add bottstrap and jquery to project and start writing click function for #Seattle
    co-authored-by: Brandon Magofna <Magofna68@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,28 @@ let place2 = new Place("Portland", "August", "Burnside skate park");
 let place3 = new Place("Salem", "August", "State Capitol");
 let place4 = new Place("New York", "May", "The Stock Exchange");
 let place5 = new Place("London", "June", "Apple Records");
+
+
+// UI logic
+$(function () {
+  $("#Seattle").on("click", function () {
+    $("#sea").text(place1);
+    console.log("click");
+    $("#sea").slideToggle()
+  });
+});
+
+
+// // "$ = JQuery; interchangable"
+// $(function() { // updated syntax: (document).ready
+//   $(".clickable.section1").on("click", function() {
+//     $("#content1").slideToggle()
+//     $("#hide1").fadeToggle()
+//     $("#show1").slideToggle()
+//   })
+//   $(".clickable.section2").on("click", function() {
+//     $("#content2").slideToggle()
+//     $("#hide2").fadeToggle()
+//     $("#show2").slideToggle()
+//   })
+// })
